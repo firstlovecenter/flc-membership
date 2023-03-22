78e0b27a2f44fdc6cb6bfb4f069e1d2c57d6b5f8
John-Dag Addy <jaedagy@gmail.com>
2023-03-22 18:06:03 +0000

    feat: scaffold initial landing page

--- web-react-ts/vite.config.ts
+++ web-react-ts/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  plugins: [react(), viteTsconfigPaths()],
 })
