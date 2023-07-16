605b6a686004c5467c6ab777873beec8af51e6df
John-Dag Addy <jaedagy@gmail.com>
2023-07-16 05:26:06 +0000

    fix: add a placeholder constant

--- web-react-ts/src/utils/constants.ts
+++ web-react-ts/src/utils/constants.ts
@@ -2,3 +2,6 @@ export const GIVING_METHODS = [
   { key: 'Mobile Money', value: 'mobileMoney' },
   { key: 'Card', value: 'card' },
 ]
+
+// TODO: remove this constant
+export const CUSTOM = 'custom'
