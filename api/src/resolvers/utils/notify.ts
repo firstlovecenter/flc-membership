8cb185e4736a2ada1cbc1089b2f9e2480b2013ee
John-Dag Addy <jaedagy@gmail.com>
2023-08-22 13:10:41 +0800

    fix: implement notify functions in api

---
+++ api/src/resolvers/utils/notify.ts
@@ -0,0 +1,92 @@
+/* eslint-disable no-console */
+import axios from 'axios'
+import { Member } from '@jaedag/admin-portal-types'
+import { throwToSentry } from './utils'
+import SECRETS from '../getSecrets'
+
+const formData = require('form-data')
+const Mailgun = require('mailgun.js')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const mailgun = new Mailgun(formData)
+const mg = mailgun.client({
+  username: 'api',
+  key: SECRETS.MAILGUN_API_KEY,
+})
+
+export const sendSingleEmail = (
+  member: Member,
+  subject: string,
+  body?: string,
+  html?: string
+) => {
+  mg.messages
+    .create(SECRETS.MAILGUN_DOMAIN, {
+      from: 'FL Accra Admin <no-reply@firstlovecenter.org>',
+      to: SECRETS.TEST_EMAIL_ADDRESS || member.email,
+      subject,
+      text: body,
+      template: '',
+      html: html || undefined, // HTML Version of the Message for Better Styling
+    })
+    .then((msg: any) => console.log('Mailgun API response', msg)) // logs response data
+    .catch((err: any) => console.log('Mailgun API error', err)) // logs any error
+}
+
+export const sendBulkEmail = (
+  recipient: string[],
+  subject: string,
+  body?: string,
+  html?: string
+) => {
+  mg.messages
+    .create(SECRETS.MAILGUN_DOMAIN, {
+      from: 'FL Accra Admin <no-reply@firstlovecenter.org>',
+      to: SECRETS.TEST_EMAIL_ADDRESS || recipient,
+      subject,
+      text: body,
+      template: '',
+      html: html || undefined, // HTML Version of the Message for Better Styling
+    })
+    .then((msg: any) => console.log('Mailgun API response', msg)) // logs response data
+    .catch((err: any) => console.log('Mailgun API error', err)) // logs any error
+}
+
+export const sendBulkSMS = async (recipient: string[], message: string) => {
+  const sendMessage = {
+    method: 'post',
+    url: `https://api.mnotify.com/api/sms/quick?key=${SECRETS.MNOTIFY_KEY}`,
+    headers: {
+      'content-type': 'application/json',
+    },
+    data: {
+      recipient: SECRETS.TEST_PHONE_NUMBER
+        ? [SECRETS.TEST_PHONE_NUMBER]
+        : recipient,
+      sender: 'FLC Admin',
+      message,
+      is_schedule: 'false',
+      schedule_date: '',
+    },
+  }
+
+  try {
+    console.log('Sending SMS using mNotify')
+    const res = await axios(sendMessage)
+
+    if (res.data.code === '2000') {
+      return 'Message sent successfully'
+    }
+    throw new Error('There was a problem sending your SMS')
+  } catch (error: any) {
+    throwToSentry('There was a problem sending your message', error)
+  }
+
+  return 'Message sent successfully'
+}
+
+export const joinMessageStrings = (strings: string[]) => {
+  return strings.join('')
+}
