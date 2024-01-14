e718ec6f163fe0db2b3f7c7bf05afb4cd679e71a
John-Dag Addy <jaedagy@gmail.com>
2024-01-14 18:56:10 +0000

    fix: update @jaedag/admin-portal-types to version 1.5.13

--- api/src/resolvers/utils/financial-utils.ts
+++ api/src/resolvers/utils/financial-utils.ts
@@ -6,21 +6,44 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 export const getStreamFinancials = (stream: Stream) => {
-  const auth = SECRETS.PAYSTACK_PRIVATE_KEY_WEEKDAY || ''
-  let subaccount = ''
+  const auth = SECRETS.PAYSTACK_PRIVATE_KEY_WEEKDAY
+  let subaccount
 
-  switch (stream?.bankAccount) {
+  switch (stream.bankAccount) {
     case 'aes_account':
-      subaccount = SECRETS.PAYSTACK_SUBACCOUNT_AES || ''
+      throw new Error(
+        'Payment Error' +
+          'Anagkazo has a different financial system. Thank you!'
+      )
+    case 'adenta_account':
+      subaccount = SECRETS.PS_SUBACCOUNT_ADENTA
       break
-    case 'ges_account':
-      subaccount = SECRETS.PAYSTACK_SUBACCOUNT_GES || ''
+    case 'kwabenya_account':
+      subaccount = SECRETS.PS_SUBACCOUNT_KWABENYA
       break
-    case 'hge_account':
-      subaccount = SECRETS.PAYSTACK_SUBACCOUNT_HGE || ''
+    case 'kwabenya_morning_account':
+      subaccount = SECRETS.PS_SUBACCOUNT_MORNING_KWABENYA
       break
     case 'fle_account':
-      subaccount = SECRETS.PAYSTACK_SUBACCOUNT_FLE || ''
+      subaccount = SECRETS.PS_SUBACCOUNT_FLE
+      break
+    case 'oa_kumasi':
+      subaccount = SECRETS.PS_SUBACCOUNT_KUMASI
+      break
+    case 'oa_gheast':
+      subaccount = SECRETS.PS_SUBACCOUNT_OA_GHEAST
+      break
+    case 'oa_ghnorth':
+      subaccount = SECRETS.PS_SUBACCOUNT_OA_GHNORTH
+      break
+    case 'oa_ghsouth':
+      subaccount = SECRETS.PS_SUBACCOUNT_OA_GHSOUTH
+      break
+    case 'oa_ghwest':
+      subaccount = SECRETS.PS_SUBACCOUNT_OA_GHWEST
+      break
+    case 'oa_tarkwa':
+      subaccount = SECRETS.PS_SUBACCOUNT_OA_TARKWA
       break
 
     default:
